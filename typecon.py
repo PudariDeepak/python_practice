8c5953d638bb4e97201cb79a3d5d35e74dbaebeb
DEEPAK PUDARI <22wj8a6743@gniindia.org>
2026-04-20 08:17:56 +0530

    first commit

---
+++ typecon.py
@@ -0,0 +1,31 @@
+#b=0
+#print(bool(b))
+
+'''a=0.0
+print(int(a))
+print(str(a))
+print(bool(a))'''
+
+'''c=" 43"
+print(int(c))
+print(float(c))
+print(bool(c))'''
+
+'''d="True"
+print(bool(d))'''
+
+
+#practice 
+a="50"
+print(int(a))
+b=25
+print(str(b))
+print("Hello "*5)
+print("Age is "+str(25))
+c="python"
+print(c[0:3])
+print(c[-2:])
+print(c[::-1])
+print(c[0::2])
+d=0
+print(bool(d))
